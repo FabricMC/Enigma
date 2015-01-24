c45dbbe43eaa9ecbfa9d59fc80a19f2aa4adb6d9
jeff <jeff@localhost>
2015-01-24 14:59:02 -0500

    switched to new and improved ssjb ssjb can handle all of the dependencies now

--- build.py
+++ build.py
@@ -1,18 +1,45 @@
 
 import os
-import ssjb
+import sys
 
 # settings
+PathSsjb = "../ssjb"
 GroupId = "cuchaz"
 ProjectName = "enigma"
 Version = "0.6b"
 Author = "Cuchaz"
 
 DirBin = "bin"
+DirLib = "lib"
 DirBuild = "build"
 DirTemp = os.path.join(DirBuild, "tmp")
 PathLocalMavenRepo = "../maven"
 
+# import ssjb
+sys.path.insert(0, PathSsjb)
+import ssjb
+import ssjb.ivy
+
+
+# dependencies
+ExtraRepos = [
+	"http://maven.cuchazinteractive.com"
+]
+Deps = [
+	ssjb.ivy.Dep("com.google.guava:guava:17.0"),
+	ssjb.ivy.Dep("de.sciss:jsyntaxpane:1.0.0"),
+	ssjb.ivy.Dep("org.javassist:javassist:3.18.1-GA"),
+	ssjb.ivy.Dep("org.bitbucket.mstrobel:procyon-decompiler:0.5.26-enigma")
+]
+ProguardDeps = [
+	ssjb.ivy.Dep("net.sf.proguard:proguard-base:5.1")
+]
+TestDeps = [
+	ssjb.ivy.Dep("junit:junit:4.12"),
+	ssjb.ivy.Dep("org.hamcrest:hamcrest-all:1.3")
+]
+
+# functions
 
 def getJarFullName(name=None) :
 	if name is not None:
@@ -53,15 +80,27 @@ def makeTestJar(name, glob):
 	pathObfJar = os.path.join(DirBuild, "%s.obf.jar" % name)
 
 	# build the deobf jar
-	ssjb.delete(DirTemp)
-	os.makedirs(DirTemp)
-	ssjb.copyFiles(DirTemp, DirBin, ssjb.findFiles(DirBin, "cuchaz/enigma/inputs/Keep.class"))
-	ssjb.copyFiles(DirTemp, DirBin, ssjb.findFiles(DirBin, glob))
-	ssjb.jar(pathJar, DirTemp)
-	ssjb.delete(DirTemp)
+	ssjb.file.delete(DirTemp)
+	ssjb.file.mkdir(DirTemp)
+	ssjb.file.copyTree(DirTemp, DirBin, ssjb.file.find(DirBin, "cuchaz/enigma/inputs/Keep.class"))
+	ssjb.file.copyTree(DirTemp, DirBin, ssjb.file.find(DirBin, glob))
+	ssjb.jar.makeJar(pathJar, DirTemp)
+	ssjb.file.delete(DirTemp)
 
 	# build the obf jar
-	ssjb.callJavaJar("libs/proguard.jar", ["@proguard.conf", "-injars", pathJar, "-outjars", pathObfJar])
+	ssjb.callJavaJar(
+		os.path.join(DirLib, "proguard.jar"),
+		["@proguard.conf", "-injars", pathJar, "-outjars", pathObfJar]
+	)
+
+
+# tasks
+
+def taskGetDeps():
+	ssjb.file.mkdir(DirLib)
+	ssjb.ivy.makeLibsJar(os.path.join(DirLib, "deps.jar"), Deps, extraRepos=ExtraRepos)
+	ssjb.ivy.makeLibsJar(os.path.join(DirLib, "test-deps.jar"), TestDeps)
+	ssjb.ivy.makeJar(os.path.join(DirLib, "proguard.jar"), ProguardDeps)
 
 def taskBuildTestJars():
 	makeTestJar("testLoneClass", "cuchaz/enigma/inputs/loneClass/*.class")
@@ -70,6 +109,7 @@ def taskBuildTestJars():
 	makeTestJar("testInnerClasses", "cuchaz/enigma/inputs/innerClasses/*.class")
 
 
+ssjb.registerTask("getDeps", taskGetDeps)
 ssjb.registerTask("main", taskMain)
 ssjb.registerTask("buildTestJars", taskBuildTestJars)
 ssjb.run()
