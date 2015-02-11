6044c91079ae416ecaff2412f8ca8653f39e6f83
jeff <jeff@localhost>
2015-02-10 22:51:55 -0500

    black list Object methods from deobfuscation

--- src/cuchaz/enigma/Deobfuscator.java
+++ src/cuchaz/enigma/Deobfuscator.java
@@ -397,6 +397,38 @@ public class Deobfuscator {
 	}
 	
 	public boolean isObfuscatedIdentifier(Entry obfEntry) {
+		
+		if (obfEntry instanceof MethodEntry) {
+			
+			// HACKHACK: Object methods are not obfuscated identifiers
+			MethodEntry obfMethodEntry = (MethodEntry)obfEntry;
+			String name = obfMethodEntry.getName();
+			String sig = obfMethodEntry.getSignature().toString();
+			if (name.equals("clone") && sig.equals("()Ljava/lang/Object;")) {
+				return false;
+			} else if (name.equals("equals") && sig.equals("(Ljava/lang/Object;)Z")) {
+				return false;
+			} else if (name.equals("finalize") && sig.equals("()V")) {
+				return false;
+			} else if (name.equals("getClass") && sig.equals("()Ljava/lang/Class;")) {
+				return false;
+			} else if (name.equals("hashCode") && sig.equals("()I")) {
+				return false;
+			} else if (name.equals("notify") && sig.equals("()V")) {
+				return false;
+			} else if (name.equals("notifyAll") && sig.equals("()V")) {
+				return false;
+			} else if (name.equals("toString") && sig.equals("()Ljava/lang/String;")) {
+				return false;
+			} else if (name.equals("wait") && sig.equals("()V")) {
+				return false;
+			} else if (name.equals("wait") && sig.equals("(J)V")) {
+				return false;
+			} else if (name.equals("wait") && sig.equals("(JI)V")) {
+				return false;
+			}
+		}
+		
 		return m_jarIndex.containsObfEntry(obfEntry);
 	}
 	
---
+++ test/cuchaz/enigma/inputs/translation/F_ObjectMethods.java
@@ -0,0 +1,19 @@
+package cuchaz.enigma.inputs.translation;
+
+public class F_ObjectMethods {
+	
+	public void callEmAll()
+	throws Throwable {
+		clone();
+		equals(this);
+		finalize();
+		getClass();
+		hashCode();
+		notify();
+		notifyAll();
+		toString();
+		wait();
+		wait(0);
+		wait(0, 0);
+	}
+}
