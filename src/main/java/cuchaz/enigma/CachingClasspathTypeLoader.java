7f914aa5d50f151ceff74f0764c9eafb2bebfd9a
Thiakil <xander@thiakil.com>
2018-07-23 13:22:33 +0800

    add support for the extra loader

--- src/main/java/cuchaz/enigma/CachingClasspathTypeLoader.java
+++ src/main/java/cuchaz/enigma/CachingClasspathTypeLoader.java
@@ -8,12 +8,24 @@ import com.strobel.assembler.metadata.ITypeLoader;
  * Caching version of {@link ClasspathTypeLoader}
  */
 public class CachingClasspathTypeLoader extends CachingTypeLoader {
+	private static ITypeLoader extraClassPathLoader = null;
+
+	public static void setExtraClassPathLoader(ITypeLoader loader){
+		extraClassPathLoader = loader;
+	}
+
 	private final ITypeLoader classpathLoader = new ClasspathTypeLoader();
 
 	protected byte[] doLoad(String className) {
 		Buffer parentBuf = new Buffer();
 		if (classpathLoader.tryLoadType(className, parentBuf)) {
 			return parentBuf.array();
+		}
+		if (extraClassPathLoader != null){
+			parentBuf.reset();
+			if (extraClassPathLoader.tryLoadType(className, parentBuf)){
+				return parentBuf.array();
+			}
 		}
 		return EMPTY_ARRAY;//need to return *something* as null means no store
 	}
