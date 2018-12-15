2de718c8f4e3892e9f6458e16618188cb0002723
Gegy <gegy1000@gmail.com>
2018-12-15 18:55:41 +0200

    Fix #78 (#85)

--- src/main/java/cuchaz/enigma/bytecode/translators/TranslationMethodVisitor.java
+++ src/main/java/cuchaz/enigma/bytecode/translators/TranslationMethodVisitor.java
@@ -147,7 +147,8 @@ public class TranslationMethodVisitor extends MethodVisitor {
 		// If we didn't receive any parameter metadata, generate it
 		if (!hasParameterMeta) {
 			List<TypeDescriptor> arguments = methodEntry.getDesc().getArgumentDescs();
-			int offset = ((ownerEntry.getAccess().getFlags() & Opcodes.ACC_INTERFACE) != 0) ? 1 : 0;
+			int flags = ownerEntry.getAccess().getFlags();
+			int offset = ((flags & Opcodes.ACC_INTERFACE) != 0 || (flags & Opcodes.ACC_ABSTRACT) != 0) ? 1 : 0;
 
 			for (int index = 0; index < arguments.size(); index++) {
 				LocalVariableEntry entry = new LocalVariableEntry(methodEntry, offset, "", true);
