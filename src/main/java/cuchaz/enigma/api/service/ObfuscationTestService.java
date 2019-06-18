1e1641d81938c48b5f01f6ab71fe4b8868a5889d
gegy1000 <gegy1000@gmail.com>
2019-06-18 21:37:15 +0200

    Use jopt for cli parsing

--- src/main/java/cuchaz/enigma/api/service/ObfuscationTestService.java
+++ src/main/java/cuchaz/enigma/api/service/ObfuscationTestService.java
@@ -1,9 +1,27 @@
 package cuchaz.enigma.api.service;
 
+import com.google.common.base.Strings;
+import cuchaz.enigma.translation.representation.entry.ClassEntry;
 import cuchaz.enigma.translation.representation.entry.Entry;
 
 public interface ObfuscationTestService extends EnigmaService {
 	EnigmaServiceType<ObfuscationTestService> TYPE = EnigmaServiceType.create("obfuscation_test");
 
 	boolean testDeobfuscated(Entry<?> entry);
+
+	final class Default implements ObfuscationTestService {
+		Default INSTANCE = new Default();
+
+		Default() {
+		}
+
+		@Override
+		public boolean testDeobfuscated(Entry<?> entry) {
+			if (entry instanceof ClassEntry) {
+				String packageName = ((ClassEntry) entry).getPackageName();
+				return Strings.isNullOrEmpty(packageName);
+			}
+			return false;
+		}
+	}
 }
