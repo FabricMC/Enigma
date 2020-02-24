0b1a5106a0eae243672b5bc1ccd73ed9e61af5d3
Yanis48 <doublecraft.official@gmail.com>
2020-02-24 09:46:00 +0100

    IT WORKS

--- src/main/java/cuchaz/enigma/utils/I18n.java
+++ src/main/java/cuchaz/enigma/utils/I18n.java
@@ -9,8 +9,12 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Stream;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Maps;
+import com.google.common.reflect.ClassPath;
+import com.google.common.reflect.ClassPath.ResourceInfo;
 import com.google.gson.Gson;
 
 import cuchaz.enigma.config.Config;
@@ -68,21 +72,32 @@ public class I18n {
 	
 	public static ArrayList<String> getAvailableLanguages() {
 		ArrayList<String> list = new ArrayList<String>();
-		File dir = new File(I18n.class.getResource("/lang/").getFile());
-		for (String file : dir.list()) {
-			if (file.endsWith(".json")) {
-				String fileName = file.substring(0, file.length() - 5);
-				list.add(fileName);
-				try (InputStream inputStream = I18n.class.getResourceAsStream("/lang/" + file)) {
-					try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
-						Map<?, ?> map = GSON.fromJson(reader, Map.class);
-						languageNames.put(fileName, map.get("language").toString());
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
+		
+		try {
+			ImmutableList<ResourceInfo> resources = ClassPath.from(Thread.currentThread().getContextClassLoader()).getResources().asList();
+			Stream<ResourceInfo> dirStream = resources.stream();
+			dirStream.forEach(context -> {
+				String file = context.getResourceName();
+				if (file.endsWith(".json") && file.startsWith("lang/")) {
+					String fileName = file.substring(5, file.length() - 5);
+					list.add(fileName);
+					loadLanguagesNames(fileName);
 				}
-			}
+			});
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
 		return list;
+	}
+	
+	private static void loadLanguagesNames(String fileName) {
+		try (InputStream stream = Thread.currentThread().getContextClassLoader().getResourceAsStream("lang/" + fileName + ".json")) {
+			try (BufferedReader reader = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))) {
+				Map<?, ?> map = GSON.fromJson(reader, Map.class);
+				languageNames.put(fileName, map.get("language").toString());
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
