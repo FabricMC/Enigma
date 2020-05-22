06ed4ec9f3ead1261ad8e20e1c15666e5b75e4b9
2xsaiko <git@dblsaiko.net>
2020-05-22 11:51:52 +0200

    Make popup menu text translatable

--- enigma-swing/src/main/java/cuchaz/enigma/gui/elements/EditorTabPopupMenu.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/elements/EditorTabPopupMenu.java
@@ -9,6 +9,7 @@ import javax.swing.KeyStroke;
 
 import cuchaz.enigma.gui.Gui;
 import cuchaz.enigma.gui.panels.PanelEditor;
+import cuchaz.enigma.utils.I18n;
 
 public class EditorTabPopupMenu {
 
@@ -27,24 +28,24 @@ public class EditorTabPopupMenu {
 
 		this.ui = new JPopupMenu();
 
-		this.close = new JMenuItem("Close");
+		this.close = new JMenuItem(I18n.translate("popup_menu.editor_tab.close"));
 		this.close.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_4, KeyEvent.CTRL_DOWN_MASK));
 		this.close.addActionListener(a -> gui.closeEditor(editor));
 		this.ui.add(this.close);
 
-		this.closeAll = new JMenuItem("Close All");
+		this.closeAll = new JMenuItem(I18n.translate("popup_menu.editor_tab.close_all"));
 		this.closeAll.addActionListener(a -> gui.closeAllEditorTabs());
 		this.ui.add(this.closeAll);
 
-		this.closeOthers = new JMenuItem("Close Others");
+		this.closeOthers = new JMenuItem(I18n.translate("popup_menu.editor_tab.close_others"));
 		this.closeOthers.addActionListener(a -> gui.closeTabsExcept(editor));
 		this.ui.add(this.closeOthers);
 
-		this.closeLeft = new JMenuItem("Close All to the Left");
+		this.closeLeft = new JMenuItem(I18n.translate("popup_menu.editor_tab.close_left"));
 		this.closeLeft.addActionListener(a -> gui.closeTabsLeftOf(editor));
 		this.ui.add(this.closeLeft);
 
-		this.closeRight = new JMenuItem("Close All to the Right");
+		this.closeRight = new JMenuItem(I18n.translate("popup_menu.editor_tab.close_right"));
 		this.closeRight.addActionListener(a -> gui.closeTabsRightOf(editor));
 		this.ui.add(this.closeRight);
 	}
