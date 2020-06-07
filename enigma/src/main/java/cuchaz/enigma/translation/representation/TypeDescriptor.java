83fff333071c759f7524014ca06fd28fda40d114
2xsaiko <git@dblsaiko.net>
2020-06-07 17:56:31 +0200

    Fix crash on remapping [T

--- enigma/src/main/java/cuchaz/enigma/translation/representation/TypeDescriptor.java
+++ enigma/src/main/java/cuchaz/enigma/translation/representation/TypeDescriptor.java
@@ -11,17 +11,18 @@
 
 package cuchaz.enigma.translation.representation;
 
-import com.google.common.base.Preconditions;
-import com.google.common.collect.Maps;
-import cuchaz.enigma.translation.Translatable;
-import cuchaz.enigma.translation.Translator;
-import cuchaz.enigma.translation.mapping.EntryMapping;
-import cuchaz.enigma.translation.mapping.EntryResolver;
-import cuchaz.enigma.translation.mapping.EntryMap;
-import cuchaz.enigma.translation.representation.entry.ClassEntry;
-
 import java.util.Map;
 import java.util.function.Function;
+
+import com.google.common.base.Preconditions;
+import com.google.common.collect.Maps;
+
+import cuchaz.enigma.translation.Translatable;
+import cuchaz.enigma.translation.Translator;
+import cuchaz.enigma.translation.mapping.EntryMap;
+import cuchaz.enigma.translation.mapping.EntryMapping;
+import cuchaz.enigma.translation.mapping.EntryResolver;
+import cuchaz.enigma.translation.representation.entry.ClassEntry;
 
 public class TypeDescriptor implements Translatable {
 
@@ -32,7 +33,7 @@ public class TypeDescriptor implements Translatable {
 
 		// don't deal with generics
 		// this is just for raw jvm types
-		if (desc.charAt(0) == 'T' || desc.indexOf('<') >= 0 || desc.indexOf('>') >= 0) {
+		if ((desc.charAt(0) == 'T' && readClass(desc) != null) || desc.indexOf('<') >= 0 || desc.indexOf('>') >= 0) {
 			throw new IllegalArgumentException("don't use with generic types or templates: " + desc);
 		}
 
