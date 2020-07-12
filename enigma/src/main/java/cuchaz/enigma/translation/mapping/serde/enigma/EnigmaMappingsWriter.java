aa8876f908432d2b6fdbfd25ace14084af7150b6
Thiakil <xander@thiakil.com>
2020-07-12 12:30:54 +0800

    Fix Enigma Zip writer ZipFS exception on non-matching filesystems

--- enigma/src/main/java/cuchaz/enigma/translation/mapping/serde/enigma/EnigmaMappingsWriter.java
+++ enigma/src/main/java/cuchaz/enigma/translation/mapping/serde/enigma/EnigmaMappingsWriter.java
@@ -148,7 +148,7 @@ public enum EnigmaMappingsWriter implements MappingsWriter {
 		private void deleteDeadPackages(Path root, Path packagePath) throws IOException {
 			for (int i = packagePath.getNameCount() - 1; i >= 0; i--) {
 				Path subPath = packagePath.subpath(0, i + 1);
-				Path packagePart = root.resolve(subPath);
+				Path packagePart = root.resolve(subPath.toString());
 				if (isEmpty(packagePart)) {
 					Files.deleteIfExists(packagePart);
 				}
