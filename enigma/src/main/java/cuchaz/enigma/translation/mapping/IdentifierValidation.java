fb87ca043b8d1b83b69dadb86e9584707b1af408
2xsaiko <git@dblsaiko.net>
2020-12-20 12:23:13 +0100

    Add true, false and null to invalid identifiers

--- enigma/src/main/java/cuchaz/enigma/translation/mapping/IdentifierValidation.java
+++ enigma/src/main/java/cuchaz/enigma/translation/mapping/IdentifierValidation.java
@@ -28,7 +28,7 @@ public final class IdentifierValidation {
 			"boolean", "do", "if", "private", "this", "break", "double", "implements", "protected", "throw", "byte",
 			"else", "import", "public", "throws", "case", "enum", "instanceof", "return", "transient", "catch",
 			"extends", "int", "short", "try", "char", "final", "interface", "static", "void", "class", "finally",
-			"long", "strictfp", "volatile", "const", "float", "native", "super", "while", "_"
+			"long", "strictfp", "volatile", "const", "float", "native", "super", "while", "true", "false", "null", "_"
 	);
 
 	public static boolean validateClassName(ValidationContext vc, String name, boolean isInner) {
