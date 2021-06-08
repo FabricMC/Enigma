ed3ad1ee8902e1fcf6828cbfe9d5814779fb5ab5
Player <player@player.to>
2021-06-08 13:16:57 +0200

    Work around SourceIndexMethodVisitor tripping over missing MEMBER_REFERENCE data for invokedynamic, print decompilation exceptions

--- enigma/src/main/java/cuchaz/enigma/source/procyon/index/SourceIndexMethodVisitor.java
+++ enigma/src/main/java/cuchaz/enigma/source/procyon/index/SourceIndexMethodVisitor.java
@@ -41,24 +41,26 @@ public class SourceIndexMethodVisitor extends SourceIndexVisitor {
 	public Void visitInvocationExpression(InvocationExpression node, SourceIndex index) {
 		MemberReference ref = node.getUserData(Keys.MEMBER_REFERENCE);
 
-		// get the behavior entry
-		ClassEntry classEntry = new ClassEntry(ref.getDeclaringType().getInternalName());
-		MethodEntry methodEntry = null;
-		if (ref instanceof MethodReference) {
-			methodEntry = new MethodEntry(classEntry, ref.getName(), new MethodDescriptor(ref.getErasedSignature()));
-		}
-		if (methodEntry != null) {
-			// get the node for the token
-			AstNode tokenNode = null;
-			if (node.getTarget() instanceof MemberReferenceExpression) {
-				tokenNode = ((MemberReferenceExpression) node.getTarget()).getMemberNameToken();
-			} else if (node.getTarget() instanceof SuperReferenceExpression) {
-				tokenNode = node.getTarget();
-			} else if (node.getTarget() instanceof ThisReferenceExpression) {
-				tokenNode = node.getTarget();
+		if (ref != null) {
+			// get the behavior entry
+			ClassEntry classEntry = new ClassEntry(ref.getDeclaringType().getInternalName());
+			MethodEntry methodEntry = null;
+			if (ref instanceof MethodReference) {
+				methodEntry = new MethodEntry(classEntry, ref.getName(), new MethodDescriptor(ref.getErasedSignature()));
 			}
-			if (tokenNode != null) {
-				index.addReference(TokenFactory.createToken(index, tokenNode), methodEntry, this.methodEntry);
+			if (methodEntry != null) {
+				// get the node for the token
+				AstNode tokenNode = null;
+				if (node.getTarget() instanceof MemberReferenceExpression) {
+					tokenNode = ((MemberReferenceExpression) node.getTarget()).getMemberNameToken();
+				} else if (node.getTarget() instanceof SuperReferenceExpression) {
+					tokenNode = node.getTarget();
+				} else if (node.getTarget() instanceof ThisReferenceExpression) {
+					tokenNode = node.getTarget();
+				}
+				if (tokenNode != null) {
+					index.addReference(TokenFactory.createToken(index, tokenNode), methodEntry, this.methodEntry);
+				}
 			}
 		}
 
