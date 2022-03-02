a4ebe4f20ded93c1c781ebb18d3372809fe04627
ByMartrixx <bymartrixx@gmail.com>
2022-03-01 21:13:42 -0300

    Add a test for an issue with bridge methods

---
+++ enigma-swing/testProject/src/main/java/com/example/bridgemethodsissue/ExampleClass.java
@@ -0,0 +1,45 @@
+package com.example.bridgemethodsissue;
+
+public class ExampleClass extends SuperClass {
+    public ExampleClass(int x, int y, int z) {
+        super(x, y, z);
+    }
+
+    public ExampleClass foo() {
+        System.out.println("bar");
+        return this;
+    }
+
+    public ExampleClass foo(int x) {
+        return null;
+    }
+
+    public ExampleClass foo(int x, int y) {
+        return baz(y);
+    }
+
+    public ExampleClass bar() {
+        return new ExampleClass(getX(), -1, 0);
+    }
+
+    public ExampleClass bar(int x) {
+        return baz(-1, x);
+    }
+
+    public ExampleClass baz(int xz) {
+        return new ExampleClass(xz, getY(), getZ() + xz);
+    }
+
+    public ExampleClass baz(int xz, int y) {
+        if (y == 0) {
+            return this;
+        }
+        return new ExampleClass(getX() - xz, getY() * y, getZ() + xz);
+    }
+
+    public static class InnerSubClass extends ExampleClass {
+        public InnerSubClass(int x, int y, int z) {
+            super(x, y, z);
+        }
+    }
+}
---
+++ enigma-swing/testProject/src/main/java/com/example/bridgemethodsissue/SuperClass.java
@@ -0,0 +1,57 @@
+package com.example.bridgemethodsissue;
+
+public class SuperClass {
+    private int x;
+    private int y;
+    private int z;
+
+    public SuperClass(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
+
+    public int getZ() {
+        return this.z;
+    }
+
+    public SuperClass foo() {
+        System.out.println("foo");
+        return this;
+    }
+
+    public SuperClass foo(int x) {
+        return new SuperClass(x, 0, 0);
+    }
+
+    public SuperClass foo(int x, int y) {
+        return bar(1);
+    }
+
+    public SuperClass bar() {
+        return new SuperClass(0, 0, 0);
+    }
+
+    public SuperClass bar(int x) {
+        return baz(1, x);
+    }
+
+    public SuperClass baz(int xz) {
+        return new SuperClass(xz, 0, xz);
+    }
+
+    public SuperClass baz(int xz, int y) {
+        if (y == 0) {
+            return this;
+        }
+        return new SuperClass(getX(), y, xz);
+    }
+}
