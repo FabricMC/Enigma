04686bec55364555718da7672517f02724e6dc79
modmuss50 <modmuss50@gmail.com>
2022-04-13 23:06:00 +0100

    Fix memory leak with CFR

--- enigma/src/main/java/cuchaz/enigma/source/cfr/CfrSource.java
+++ enigma/src/main/java/cuchaz/enigma/source/cfr/CfrSource.java
@@ -4,34 +4,39 @@ import cuchaz.enigma.source.Source;
 import cuchaz.enigma.source.SourceIndex;
 import cuchaz.enigma.source.SourceSettings;
 import cuchaz.enigma.translation.mapping.EntryRemapper;
+import org.benf.cfr.reader.apiunreleased.ClassFileSource2;
 import org.benf.cfr.reader.entities.ClassFile;
+import org.benf.cfr.reader.mapping.MappingFactory;
+import org.benf.cfr.reader.mapping.ObfuscationMapping;
+import org.benf.cfr.reader.relationship.MemberNameResolver;
 import org.benf.cfr.reader.state.DCCommonState;
-import org.benf.cfr.reader.state.TypeUsageInformation;
+import org.benf.cfr.reader.state.TypeUsageCollectingDumper;
+import org.benf.cfr.reader.util.CannotLoadClassException;
+import org.benf.cfr.reader.util.collections.ListFactory;
 import org.benf.cfr.reader.util.getopt.Options;
+import org.benf.cfr.reader.util.getopt.OptionsImpl;
 import org.checkerframework.checker.nullness.qual.Nullable;
 
 public class CfrSource implements Source {
+    private final String className;
     private final SourceSettings settings;
-    private final ClassFile tree;
-    private final DCCommonState state;
-    private final TypeUsageInformation typeUsage;
     private final Options options;
+    private final ClassFileSource2 classFileSource;
     private final EntryRemapper mapper;
 
     private SourceIndex index;
 
-    public CfrSource(SourceSettings settings, ClassFile tree, DCCommonState state, TypeUsageInformation typeUsage, Options options, @Nullable EntryRemapper mapper) {
+    public CfrSource(String className, SourceSettings settings, Options options, ClassFileSource2 classFileSource, @Nullable EntryRemapper mapper) {
+        this.className = className;
         this.settings = settings;
-        this.tree = tree;
-        this.state = state;
-        this.typeUsage = typeUsage;
         this.options = options;
+        this.classFileSource = classFileSource;
         this.mapper = mapper;
     }
 
     @Override
     public Source withJavadocs(EntryRemapper mapper) {
-        return new CfrSource(settings, tree, state, typeUsage, options, mapper);
+        return new CfrSource(className, settings, options, classFileSource, mapper);
     }
 
     @Override
@@ -46,12 +51,36 @@ public class CfrSource implements Source {
         return index.getSource();
     }
 
-    private synchronized void ensureDecompiled() {
+    private void ensureDecompiled() {
         if (index != null) {
             return;
         }
 
-        EnigmaDumper dumper = new EnigmaDumper(new StringBuilder(), settings, typeUsage, options, mapper);
+        DCCommonState commonState = new DCCommonState(options, classFileSource);
+        ObfuscationMapping mapping = MappingFactory.get(options, commonState);
+        DCCommonState state = new DCCommonState(commonState, mapping);
+        ClassFile tree = state.getClassFileMaybePath(className);
+
+        state.configureWith(tree);
+
+        // To make sure we're analysing the cached version
+        try {
+            tree = state.getClassFile(tree.getClassType());
+        } catch (CannotLoadClassException ignored) {
+        }
+
+        if (options.getOption(OptionsImpl.DECOMPILE_INNER_CLASSES)) {
+            tree.loadInnerClasses(state);
+        }
+
+        if (options.getOption(OptionsImpl.RENAME_DUP_MEMBERS)) {
+            MemberNameResolver.resolveNames(state, ListFactory.newList(state.getClassCache().getLoadedTypes()));
+        }
+
+        TypeUsageCollectingDumper typeUsageCollector = new TypeUsageCollectingDumper(options, tree);
+        tree.analyseTop(state, typeUsageCollector);
+
+        EnigmaDumper dumper = new EnigmaDumper(new StringBuilder(), settings, typeUsageCollector.getRealTypeUsageInformation(), options, mapper);
         tree.dump(state.getObfuscationMapping().wrap(dumper));
         index = dumper.getIndex();
     }
