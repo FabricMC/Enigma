c34f422737bf2a550a1c7ac87120dfc6e210926d
Basique <basiqueevangelist@yandex.ru>
2023-06-15 17:29:12 +0300

    optimize NestedPackages sorting

--- enigma-swing/src/main/java/cuchaz/enigma/gui/NestedPackages.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/NestedPackages.java
@@ -2,23 +2,21 @@ package cuchaz.enigma.gui;
 
 import java.util.Collection;
 import java.util.Comparator;
-import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.swing.tree.DefaultMutableTreeNode;
-import javax.swing.tree.MutableTreeNode;
 import javax.swing.tree.TreeNode;
 import javax.swing.tree.TreePath;
 
 import cuchaz.enigma.gui.node.ClassSelectorClassNode;
 import cuchaz.enigma.gui.node.ClassSelectorPackageNode;
+import cuchaz.enigma.gui.node.SortedMutableTreeNode;
 import cuchaz.enigma.translation.mapping.EntryRemapper;
 import cuchaz.enigma.translation.representation.entry.ClassEntry;
 
 public class NestedPackages {
-	private final DefaultMutableTreeNode root = new DefaultMutableTreeNode();
-	private final Map<String, DefaultMutableTreeNode> packageToNode = new HashMap<>();
+	private final SortedMutableTreeNode root;
+	private final Map<String, SortedMutableTreeNode> packageToNode = new HashMap<>();
 	private final Map<ClassEntry, ClassSelectorClassNode> classToNode = new HashMap<>();
 	private final EntryRemapper remapper;
 	private final Comparator<TreeNode> comparator;
@@ -42,6 +40,7 @@ public class NestedPackages {
 
 			return 0;
 		};
+		this.root = new SortedMutableTreeNode(comparator);
 
 		for (ClassEntry entry : entries) {
 			addEntry(entry);
@@ -52,31 +51,31 @@ public class NestedPackages {
 		ClassEntry translated = remapper.deobfuscate(entry);
 		var me = new ClassSelectorClassNode(entry, translated);
 		classToNode.put(entry, me);
-		insert(getPackage(translated.getPackageName()), me);
+		getPackage(translated.getPackageName()).insert(me, 0);
 	}
 
-	public DefaultMutableTreeNode getPackage(String packageName) {
-		DefaultMutableTreeNode node = packageToNode.get(packageName);
+	public SortedMutableTreeNode getPackage(String packageName) {
+		SortedMutableTreeNode node = packageToNode.get(packageName);
 
 		if (packageName == null) {
 			return root;
 		}
 
 		if (node == null) {
-			node = new ClassSelectorPackageNode(packageName);
-			insert(getPackage(ClassEntry.getParentPackage(packageName)), node);
+			node = new ClassSelectorPackageNode(this.comparator, packageName);
+			getPackage(ClassEntry.getParentPackage(packageName)).insert(node, 0);
 			packageToNode.put(packageName, node);
 		}
 
 		return node;
 	}
 
-	public DefaultMutableTreeNode getRoot() {
+	public SortedMutableTreeNode getRoot() {
 		return root;
 	}
 
 	public TreePath getPackagePath(String packageName) {
-		DefaultMutableTreeNode node = packageToNode.getOrDefault(packageName, root);
+		SortedMutableTreeNode node = packageToNode.getOrDefault(packageName, root);
 		return new TreePath(node.getPath());
 	}
 
@@ -90,11 +89,11 @@ public class NestedPackages {
 		if (node != null) {
 			node.removeFromParent();
 			// remove dangling packages
-			DefaultMutableTreeNode packageNode = packageToNode.get(entry.getPackageName());
+			SortedMutableTreeNode packageNode = packageToNode.get(entry.getPackageName());
 
 			while (packageNode != null && packageNode.getChildCount() == 0) {
-				DefaultMutableTreeNode theNode = packageNode;
-				packageNode = (DefaultMutableTreeNode) packageNode.getParent();
+				SortedMutableTreeNode theNode = packageNode;
+				packageNode = (SortedMutableTreeNode) packageNode.getParent();
 				theNode.removeFromParent();
 
 				if (theNode instanceof ClassSelectorPackageNode pn) {
@@ -104,22 +103,7 @@ public class NestedPackages {
 		}
 	}
 
-	public Collection<DefaultMutableTreeNode> getPackageNodes() {
+	public Collection<SortedMutableTreeNode> getPackageNodes() {
 		return packageToNode.values();
-	}
-
-	private void insert(DefaultMutableTreeNode parent, MutableTreeNode child) {
-		int index = 0;
-		Enumeration<TreeNode> children = parent.children();
-
-		while (children.hasMoreElements()) {
-			if (comparator.compare(children.nextElement(), child) < 0) {
-				index++;
-			} else {
-				break;
-			}
-		}
-
-		parent.insert(child, index);
 	}
 }
