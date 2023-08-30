7b5257f7f39bc9adba16d0c63a8f70904fe0fff9
Julian Burner <48808497+NebelNidas@users.noreply.github.com>
2023-08-30 16:48:47 +0200

    Fix renaming classes via their constructor (#520)
    * Fix `Reset to obfuscated` on constructors
    
    * Fix renaming classes via their constructor
    
    * Fix `Reset to obfuscated` not getting updated on constructors
    
    * Fix checkstyle
    
    * Remove unnecessary cast

--- enigma-swing/src/main/java/cuchaz/enigma/gui/Gui.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/Gui.java
@@ -418,7 +418,7 @@ public class Gui {
 			return;
 		}
 
-		Entry<?> obfEntry = cursorReference.entry;
+		Entry<?> obfEntry = cursorReference.getNameableEntry();
 		toggleMappingFromEntry(obfEntry);
 	}
 
