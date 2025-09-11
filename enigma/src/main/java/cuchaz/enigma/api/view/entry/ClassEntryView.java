b50973d49e270480e6483eb6ddde443ff7cf10c5
Joe <burtonjae@hotmail.co.uk>
2025-09-11 15:56:11 +0100

    Add factory methods to the entry views

--- enigma/src/main/java/cuchaz/enigma/api/view/entry/ClassEntryView.java
+++ enigma/src/main/java/cuchaz/enigma/api/view/entry/ClassEntryView.java
@@ -1,5 +1,11 @@
 package cuchaz.enigma.api.view.entry;
 
+import cuchaz.enigma.translation.representation.entry.ClassEntry;
+
 public interface ClassEntryView extends EntryView {
 	ClassEntryView getParent();
+
+	static ClassEntryView create(String className) {
+		return new ClassEntry(className);
+	}
 }
