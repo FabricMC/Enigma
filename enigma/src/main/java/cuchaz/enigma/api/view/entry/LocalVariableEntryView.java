d89e2e29e95c6a4c02419eb6457714af51418834
joe <burtonjae@hotmail.co.uk>
2025-10-11 14:46:01 +0100

    Add API to create a LocalVariableEntryView

--- enigma/src/main/java/cuchaz/enigma/api/view/entry/LocalVariableEntryView.java
+++ enigma/src/main/java/cuchaz/enigma/api/view/entry/LocalVariableEntryView.java
@@ -1,9 +1,16 @@
 package cuchaz.enigma.api.view.entry;
 
+import cuchaz.enigma.translation.representation.entry.LocalVariableEntry;
+import cuchaz.enigma.translation.representation.entry.MethodEntry;
+
 public interface LocalVariableEntryView extends EntryView {
 	int getIndex();
 
 	boolean isArgument();
 
 	MethodEntryView getParent();
+
+	static LocalVariableEntryView create(MethodEntryView parent, int index, boolean argument) {
+		return new LocalVariableEntry((MethodEntry) parent, index, null, argument, null);
+	}
 }
