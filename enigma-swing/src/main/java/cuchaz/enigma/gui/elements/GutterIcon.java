75369bc7e916752e81356e290a74b45938a7a032
joe <burtonjae@hotmail.co.uk>
2025-10-15 21:29:51 +0100

    Refactor EnigmaIcon as per Juuz's suggestions

--- enigma-swing/src/main/java/cuchaz/enigma/gui/elements/GutterIcon.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/elements/GutterIcon.java
@@ -1,20 +1,30 @@
 package cuchaz.enigma.gui.elements;
 
 import java.awt.Cursor;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 
 import javax.swing.JButton;
 
 import cuchaz.enigma.api.service.GuiService;
 import cuchaz.enigma.gui.util.EnigmaIconImpl;
+import cuchaz.enigma.gui.util.ScaleUtil;
 
 public class GutterIcon extends JButton implements GuiService.GutterMarkerBuilder {
 	private Runnable clickAction = () -> { };
 
 	public GutterIcon(EnigmaIconImpl icon) {
-		super(icon);
+		super(icon.icon());
 		setContentAreaFilled(false);
 		setCursor(Cursor.getDefaultCursor());
 		addActionListener(e -> clickAction.run());
+
+		addComponentListener(new ComponentAdapter() {
+			@Override
+			public void componentResized(ComponentEvent e) {
+				setIcon(icon.icon().derive(ScaleUtil.invert(getWidth()), ScaleUtil.invert(getHeight())));
+			}
+		});
 	}
 
 	@Override
--- enigma-swing/src/main/java/cuchaz/enigma/gui/util/IconLoadingServiceImpl.java
+++ enigma-swing/src/main/java/cuchaz/enigma/gui/util/IconLoadingServiceImpl.java
@@ -3,8 +3,7 @@ package cuchaz.enigma.gui.util;
 import java.io.IOException;
 import java.io.InputStream;
 
-import com.github.weisj.jsvg.SVGDocument;
-import com.github.weisj.jsvg.parser.SVGLoader;
+import com.formdev.flatlaf.extras.FlatSVGIcon;
 
 import cuchaz.enigma.api.EnigmaIcon;
 import cuchaz.enigma.utils.IconLoadingService;
@@ -12,12 +11,6 @@ import cuchaz.enigma.utils.IconLoadingService;
 public class IconLoadingServiceImpl implements IconLoadingService {
 	@Override
 	public EnigmaIcon loadIcon(InputStream in) throws IOException {
-		SVGDocument document = new SVGLoader().load(in);
-
-		if (document == null) {
-			throw new IOException("Could not load SVG document");
-		}
-
-		return new EnigmaIconImpl(document);
+		return new EnigmaIconImpl(new FlatSVGIcon(in));
 	}
 }
